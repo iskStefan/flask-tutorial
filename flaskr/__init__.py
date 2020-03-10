6e90b28c643959baadf2280c06c5dead2f1515c7
Stefan Klemm <stefan.klemm@manrolandgoss.com>
2020-03-10 10:22:52 +0100

    Datenbank hinzugefügt

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,3 @@
-
 import os
 
 from flask import Flask
@@ -29,5 +28,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
